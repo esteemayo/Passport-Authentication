0655dfadad1d40a779234a8191298b72ef930366
Emmanuel Adebayo <eadebayo15@gmail.com>
2020-12-18 13:13:30 +0100

    Modified

--- startup/db.js
+++ startup/db.js
@@ -2,20 +2,13 @@ const mongoose = require('mongoose');
 const config = require('config');
 
 module.exports = () => {
-    // DB CONFIG
-    // const db = require('./config/keys').MongoURI;
+    const db = config.get('db');
 
-    // CONNECT TO MONGO
-    // mongoose.connect(db, {useNewUrlParser: true})
-    //     .then(() => console.log('MongoDB Connected...'))
-    //     .catch(err => console.log(err));
-
-    mongoose.connect(config.get('db'), {
+    mongoose.connect(db, {
         useNewUrlParser: true,
         useCreateIndex: true,
         useUnifiedTopology: true,
         useFindAndModify: false
     })
-        .then(() => console.log('MongoDB Connected.....'))
-        .catch(err => console.log(`COULD NOT CONNECT TO MONGODB: ${err}`));
+        .then(() => console.log(`Connected to MongoDB ———> ${db}`));
 }
